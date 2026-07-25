2331982be471b4c01b395b5130eadcee39ab0a7e
RAJITH S <103199809+Rajith-07@users.noreply.github.com>
2026-07-25 10:28:57 +0530

    Time: 0 ms (100%), Space: 8.7 MB (59.34%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
     int maxProduct(int n) {
-        int x=0, y=0;
+        // int x=0, y=0;
 
-        for (; n; n/=10) {
-            int d = n%10;
-            if (d>=x) {
-                y=x;
-                x=d;
-            } else if (d>y) {
-                y=d;
-            }
+        // for (; n; n/=10) {
+        //     int d = n%10;
+        //     if (d>=x) {
+        //         y=x;
+        //         x=d;
+        //     } else if (d>y) {
+        //         y=d;
+        //     }
+        // }
+
+        // return x*y;
+
+        uint64_t m = 0;
+        for (;n;n/=10) {
+            m += 1ULL << (n%10 << 2); // n%10 * 4
         }
 
-        return x*y;
+        int u = (bit_width(m)-1) >> 2; // highest set bit value -> /4 gives the req digit
+        return u * ((bit_width(m - (1ULL << (u<<2))) - 1) >> 2);
     }
 };
