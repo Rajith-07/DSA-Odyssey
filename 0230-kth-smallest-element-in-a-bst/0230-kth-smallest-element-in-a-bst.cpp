fbc477596e19deacd29356ae76867ad06e3f8240
RAJITH S <103199809+Rajith-07@users.noreply.github.com>
2026-08-12 09:34:55 +0530

    Time: 0 ms (100%), Space: 24.4 MB (40.91%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -9,21 +9,52 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     int kthSmallest(TreeNode* root, int k) {
+//         vector<int> res;
+//         dfs(root, res);
+//         sort(res.begin(), res.end());
+//         return res[k-1];
+//     }
+
+// private:
+//     void dfs(TreeNode* node, vector<int>& arr) {
+//         if (!node) return;
+
+//         arr.push_back(node->val);
+//         dfs(node->left, arr);
+//         dfs(node->right, arr);
+//     }
+// };
+
+
+
+// OPTIMAL DFS -> inorder traversal always gives a sorted form ->. so stopping at k gives the solution
 class Solution {
 public:
     int kthSmallest(TreeNode* root, int k) {
-        vector<int> res;
+        vector<int> res(2);
+        res[0] = k;
         dfs(root, res);
-        sort(res.begin(), res.end());
-        return res[k-1];
+        return res[1];
     }
 
 private:
-    void dfs(TreeNode* node, vector<int>& arr) {
+    void dfs(TreeNode* node, vector<int>& res) {
         if (!node) return;
 
-        arr.push_back(node->val);
-        dfs(node->left, arr);
-        dfs(node->right, arr);
+        dfs(node->left, res);
+
+        if (res[0]==0) return;
+
+        res[0]--;
+        if(res[0]==0) {
+            res[1] = node->val;
+            return;
+        }
+
+        dfs(node->right, res);
+
     }
 };
