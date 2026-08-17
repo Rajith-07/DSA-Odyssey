1fe8b0841708bafedceb4a495e33bf3e63bac550
RAJITH S <103199809+Rajith-07@users.noreply.github.com>
2026-08-17 16:06:33 +0530

    Time: 0 ms (100%), Space: 7.7 MB (99%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -14,19 +14,37 @@
 //     }
 // };
 
+
+// DP (BOTTOM-UP APPROACH)
+// class Solution {
+// public:
+//     int climbStairs(int n) {
+//         if (n<=2) return n;
+
+//         vector<int> dp(n+1);
+//         dp[1] = 1;
+//         dp[2] = 2;
+
+//         for (int i=3;i<=n;i++) {
+//             dp[i] = dp[i-1]+dp[i-2];
+//         }
+
+//         return dp[n];
+//     }
+// };
+
 class Solution {
 public:
     int climbStairs(int n) {
         if (n<=2) return n;
-
-        vector<int> dp(n+1);
-        dp[1] = 1;
-        dp[2] = 2;
+        int minus_one=2, minus_two=1, curr=0;
 
         for (int i=3;i<=n;i++) {
-            dp[i] = dp[i-1]+dp[i-2];
+            curr = minus_one + minus_two;
+            minus_two = minus_one;
+            minus_one = curr;
         }
 
-        return dp[n];
+        return curr;
     }
 };
